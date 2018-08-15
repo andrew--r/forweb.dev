4b1bfc3742a0507e6a7a72c1747e4de1eacb77ed
Andrew Romanov <me@andrew-r.ru>
2018-08-15 22:07:25 +0300

    [#6] Build and minify CSS

--- build.config.js
+++ build.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  sourcePath: path.resolve(__dirname, './source'),
+  compiledSourcePath: path.resolve(__dirname, './lib'),
   buildPath: path.resolve(__dirname, './build'),
 };
