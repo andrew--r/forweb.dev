aeea8210cb220bf9aab1758e06f2d02465bd3039
Andrew Romanov <me@andrew-r.ru>
2018-11-26 22:05:55 +0300

    Add digest widget to articles

--- source/pages/blog/principles-for-writing-idiomatic-html/template.js
+++ source/pages/blog/principles-for-writing-idiomatic-html/template.js
@@ -1,83 +1,83 @@
 const layout = require('../../../partials/layout/template');
+const article = require('../../../partials/article/template');
 
-const renderPage = () => `
-<article>
-  <h2 class="heading heading--1">
-    Принципы написания однородного HTML
-  </h2>
+const renderArticleContent = () => `
+<h2 class="heading heading--1">
+  Принципы написания однородного HTML
+</h2>
 
-  <p class="paragraph paragraph--lead">
-    Этот документ представляет собой общие рекомендации по&nbsp;стилю написания HTML-разметки. Данное руководство призывает к&nbsp;использованию общепринятых и&nbsp;устоявшихся подходов к&nbsp;написанию кода.
-  </p>
+<p class="paragraph paragraph--lead">
+  Этот документ представляет собой общие рекомендации по&nbsp;стилю написания HTML-разметки. Данное руководство призывает к&nbsp;использованию общепринятых и&nbsp;устоявшихся подходов к&nbsp;написанию кода.
+</p>
 
-  <h3 class="heading heading--2">1. Основные принципы</h3>
+<h3 class="heading heading--2">1. Основные принципы</h3>
 
-  <ul class="list">
-    <li class="list__item">
-      Весь код в&nbsp;любой базе исходников должен выглядеть так, будто его написал один человек, вне зависимости от&nbsp;реального количества разработчиков.
-    </li>
-    <li class="list__item">
-      Строго следуйте принятому стилю.
-    </li>
-    <li class="list__item">
-      Если вы&nbsp;сомневаетесь в&nbsp;том, какой стиль использовать, пользуйтесь общепринятыми и&nbsp;устоявшимися подходами.
-    </li>
-  </ul>
+<ul class="list">
+  <li class="list__item">
+    Весь код в&nbsp;любой базе исходников должен выглядеть так, будто его написал один человек, вне зависимости от&nbsp;реального количества разработчиков.
+  </li>
+  <li class="list__item">
+    Строго следуйте принятому стилю.
+  </li>
+  <li class="list__item">
+    Если вы&nbsp;сомневаетесь в&nbsp;том, какой стиль использовать, пользуйтесь общепринятыми и&nbsp;устоявшимися подходами.
+  </li>
+</ul>
 
-  <h3 class="heading heading--2">2. Пустое пространство</h3>
-  <p class="paragraph">
-    В&nbsp;вашей базе исходных кодов должен существовать лишь один стиль написания кода. Всегда согласованно используйте пустое пространство. Используйте пустое пространство для улучшения читаемости кода.
-  </p>
+<h3 class="heading heading--2">2. Пустое пространство</h3>
+<p class="paragraph">
+  В&nbsp;вашей базе исходных кодов должен существовать лишь один стиль написания кода. Всегда согласованно используйте пустое пространство. Используйте пустое пространство для улучшения читаемости кода.
+</p>
 
-  <ul class="list">
-    <li class="list__item">
-      Никогда не&nbsp;смешивайте табы и&nbsp;пробелы в&nbsp;отступах.
-    </li>
-    <li class="list__item">
-      Выберите между мягкой табуляцией (пробелы) и&nbsp;реальными табами. Придерживайтесь выбранного подхода. (Предпочтительно: мягкая табуляция)
-    </li>
-    <li class="list__item">
-      Если вы&nbsp;используете пробелы, выберите количество символов для отступа. (Предпочтительно: 4&nbsp;пробела)
-    </li>
-  </ul>
+<ul class="list">
+  <li class="list__item">
+    Никогда не&nbsp;смешивайте табы и&nbsp;пробелы в&nbsp;отступах.
+  </li>
+  <li class="list__item">
+    Выберите между мягкой табуляцией (пробелы) и&nbsp;реальными табами. Придерживайтесь выбранного подхода. (Предпочтительно: мягкая табуляция)
+  </li>
+  <li class="list__item">
+    Если вы&nbsp;используете пробелы, выберите количество символов для отступа. (Предпочтительно: 4&nbsp;пробела)
+  </li>
+</ul>
 
-  <p class="paragraph">
-    Совет: настройте свой текстовый редактор так, чтобы он&nbsp;показывал непечатные символы. Это позволит вам устранить появление лишних пробелов и&nbsp;табов в&nbsp;конце строк или ещё где-либо.
-  </p>
+<p class="paragraph">
+  Совет: настройте свой текстовый редактор так, чтобы он&nbsp;показывал непечатные символы. Это позволит вам устранить появление лишних пробелов и&nbsp;табов в&nbsp;конце строк или ещё где-либо.
+</p>
 
-  <h3 class="heading heading--2">3. Форматирование</h3>
-  <ul class="list">
-    <li class="list__item">
-      Названия тегов и&nbsp;атрибутов всегда пишите в&nbsp;нижнем регистре.
-    </li>
-    <li class="list__item">
-      На&nbsp;одной строке размещайте только один элемент.
-    </li>
-    <li class="list__item">
-      Используйте дополнительный уровень отступов для каждого вложенного элемента.
-    </li>
-    <li class="list__item">
-      Указывайте атрибуты без их&nbsp;значения, если это возможно (например, <code>checked</code> вместо <code>checked="checked"</code>).
-    </li>
-    <li class="list__item">
-      Всегда используйте двойные кавычки для значений атрибутов.
-    </li>
-    <li class="list__item">
-      Не&nbsp;указывайте атрибут <code>type</code> в&nbsp;элементах <code>link</code>, <code>style</code> и <code>script</code>.
-    </li>
-    <li class="list__item">Всегда закрывайте теги.</li>
-    <li class="list__item">
-      Не&nbsp;используйте слеш в&nbsp;единичных тегах (<code><img></code> &mdash; правильно, <code><img/></code> &mdash; неправильно).
-    </li>
-  </ul>
+<h3 class="heading heading--2">3. Форматирование</h3>
+<ul class="list">
+  <li class="list__item">
+    Названия тегов и&nbsp;атрибутов всегда пишите в&nbsp;нижнем регистре.
+  </li>
+  <li class="list__item">
+    На&nbsp;одной строке размещайте только один элемент.
+  </li>
+  <li class="list__item">
+    Используйте дополнительный уровень отступов для каждого вложенного элемента.
+  </li>
+  <li class="list__item">
+    Указывайте атрибуты без их&nbsp;значения, если это возможно (например, <code>checked</code> вместо <code>checked="checked"</code>).
+  </li>
+  <li class="list__item">
+    Всегда используйте двойные кавычки для значений атрибутов.
+  </li>
+  <li class="list__item">
+    Не&nbsp;указывайте атрибут <code>type</code> в&nbsp;элементах <code>link</code>, <code>style</code> и <code>script</code>.
+  </li>
+  <li class="list__item">Всегда закрывайте теги.</li>
+  <li class="list__item">
+    Не&nbsp;используйте слеш в&nbsp;единичных тегах (<code><img></code> &mdash; правильно, <code><img/></code> &mdash; неправильно).
+  </li>
+</ul>
 
-  <p class="paragraph">
-    (Сохраняйте длину строки в&nbsp;пределах разумного максимума, например 80&nbsp;символов.)
-  </p>
+<p class="paragraph">
+  (Сохраняйте длину строки в&nbsp;пределах разумного максимума, например 80&nbsp;символов.)
+</p>
 
-  <p class="paragraph">Пример:</p>
+<p class="paragraph">Пример:</p>
 
-  <pre><code>&lt;div class=&quot;tweet&quot;&gt;
+<pre><code>&lt;div class=&quot;tweet&quot;&gt;
     &lt;a href=&quot;path/to/somewhere&quot;&gt;
         &lt;img src=&quot;path/to/image.png&quot; alt=&quot;&quot;&gt;
     &lt;/a&gt;
@@ -85,64 +85,64 @@ const renderPage = () => `
     &lt;button disabled&gt;Reply&lt;/button&gt;
 &lt;/div&gt;</code></pre>
 
-  <h4 class="heading heading--3">
-    Исключения и&nbsp;небольшие допущения
-  </h4>
+<h4 class="heading heading--3">
+  Исключения и&nbsp;небольшие допущения
+</h4>
 
-  <p class="paragraph">
-    В&nbsp;элементах с&nbsp;несколькими атрибутами эти атрибуты можно расположить на&nbsp;отдельных строках, чтобы улучшить читабельность:
-  </p>
+<p class="paragraph">
+  В&nbsp;элементах с&nbsp;несколькими атрибутами эти атрибуты можно расположить на&nbsp;отдельных строках, чтобы улучшить читабельность:
+</p>
 
-  <pre><code>&lt;a class=&quot;[value]&quot;
+<pre><code>&lt;a class=&quot;[value]&quot;
  data-action=&quot;[value]&quot;
  data-id=&quot;[value]&quot;
  href=&quot;[url]&quot;&gt;
     &lt;span&gt;[text]&lt;/span&gt;
 &lt;/a&gt;</code></pre>
 
-  <h3 class="heading heading--2">4. Порядок атрибутов</h3>
+<h3 class="heading heading--2">4. Порядок атрибутов</h3>
 
-  <p class="paragraph">
-    HTML-атрибуты должны быть перечислены в&nbsp;порядке, учитывающем&nbsp;то, что названия классов&nbsp;&mdash; базовая вещь, которая позволяет выбирать элементы с&nbsp;помощью JavaScript или CSS:
-  </p>
+<p class="paragraph">
+  HTML-атрибуты должны быть перечислены в&nbsp;порядке, учитывающем&nbsp;то, что названия классов&nbsp;&mdash; базовая вещь, которая позволяет выбирать элементы с&nbsp;помощью JavaScript или CSS:
+</p>
 
-  <ol class="list">
-    <li class="list__item">
-      <code>class</code>
-    </li>
-    <li class="list__item">
-      <code>id</code>
-    </li>
-    <li class="list__item">
-      <code>data-*</code>
-    </li>
-    <li class="list__item">Любые другие атрибуты</li>
-  </ol>
+<ol class="list">
+  <li class="list__item">
+    <code>class</code>
+  </li>
+  <li class="list__item">
+    <code>id</code>
+  </li>
+  <li class="list__item">
+    <code>data-*</code>
+  </li>
+  <li class="list__item">Любые другие атрибуты</li>
+</ol>
 
-  <p class="paragraph">Пример:</p>
+<p class="paragraph">Пример:</p>
 
-  <pre><code>&lt;a class=&quot;[value]&quot; id=&quot;[value]&quot; data-name=&quot;[value]&quot; href=&quot;[url]&quot;&gt;[text]&lt;/a&gt;</code></pre>
+<pre><code>&lt;a class=&quot;[value]&quot; id=&quot;[value]&quot; data-name=&quot;[value]&quot; href=&quot;[url]&quot;&gt;[text]&lt;/a&gt;</code></pre>
 
-  <h3 class="heading heading--2">5. Именование</h3>
+<h3 class="heading heading--2">5. Именование</h3>
 
-  <p class="paragraph">
-    Именование&nbsp;&mdash; вещь сложная, но&nbsp;очень важная. Это ключевая часть процесса написания поддерживаемого кода. Без правильного именования невозможно иметь относительно масштабируемый интерфейс между HTML и&nbsp;CSS/JS.
-  </p>
+<p class="paragraph">
+  Именование&nbsp;&mdash; вещь сложная, но&nbsp;очень важная. Это ключевая часть процесса написания поддерживаемого кода. Без правильного именования невозможно иметь относительно масштабируемый интерфейс между HTML и&nbsp;CSS/JS.
+</p>
 
-  <ul class="list">
-    <li class="list__item">
-      Используйте простые, понятные и&nbsp;уместные имена для классов. Имена должны быть информативны вне зависимости от&nbsp;контекста&nbsp;&mdash; как в&nbsp;разметке, так и&nbsp;в&nbsp;CSS-файлах.
-    </li>
-    <li class="list__item">
-      Избегайте систематического сокращения имён классов. Не&nbsp;усложняйте код.
-    </li>
-  </ul>
+<ul class="list">
+  <li class="list__item">
+    Используйте простые, понятные и&nbsp;уместные имена для классов. Имена должны быть информативны вне зависимости от&nbsp;контекста&nbsp;&mdash; как в&nbsp;разметке, так и&nbsp;в&nbsp;CSS-файлах.
+  </li>
+  <li class="list__item">
+    Избегайте систематического сокращения имён классов. Не&nbsp;усложняйте код.
+  </li>
+</ul>
 
-  <p class="paragraph">Пример с&nbsp;плохим именованием классов:</p>
+<p class="paragraph">Пример с&nbsp;плохим именованием классов:</p>
 
-  <pre><code>&lt;div class=&quot;cb s-scr&quot;&gt;&lt;/div&gt;</code></pre>
+<pre><code>&lt;div class=&quot;cb s-scr&quot;&gt;&lt;/div&gt;</code></pre>
 
-  <pre><code>.s-scr {
+<pre><code>.s-scr {
   overflow: auto;
 }
 
@@ -150,11 +150,11 @@ const renderPage = () => `
   background: #000;
 }</code></pre>
 
-  <p class="paragraph">Хороший пример именования:</p>
+<p class="paragraph">Хороший пример именования:</p>
 
-  <pre><code>&lt;div class=&quot;column-body is-scrollable&quot;&gt;&lt;/div&gt;</code></pre>
+<pre><code>&lt;div class=&quot;column-body is-scrollable&quot;&gt;&lt;/div&gt;</code></pre>
 
-  <pre><code>.is-scrollable {
+<pre><code>.is-scrollable {
     overflow: auto;
 }
 
@@ -162,11 +162,11 @@ const renderPage = () => `
     background: #000;
 }</code></pre>
 
-  <h3 class="heading heading--2">6. Практический пример</h3>
+<h3 class="heading heading--2">6. Практический пример</h3>
 
-  <p class="paragraph">Пример различных соглашений.</p>
+<p class="paragraph">Пример различных соглашений.</p>
 
-  <pre><code>&lt;!DOCTYPE html&gt;
+<pre><code>&lt;!DOCTYPE html&gt;
 &lt;html&gt;
     &lt;head&gt;
         &lt;meta charset=&quot;utf-8&quot;&gt;
@@ -201,10 +201,10 @@ const renderPage = () => `
     &lt;/body&gt;
 &lt;/html&gt;</code></pre>
 
-  <p class="paragraph">
-    <i>Это перевод <a href="https://github.com/necolas/idiomatic-html">Principles of writing consistent, idiomatic HTML</a> Николаса Галлахера. Оригинал лицензирован по <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>. Лицензия распространяется на все документы и переводы.</i>
-  </p>
-</article>
+<p class="paragraph">
+  <i>Это перевод <a href="https://github.com/necolas/idiomatic-html">Principles of writing consistent, idiomatic HTML</a> Николаса Галлахера. Оригинал лицензирован по <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>. Лицензия распространяется на все документы и переводы.</i>
+</p>
 `;
 
-module.exports = (context) => layout(context, renderPage(context));
+module.exports = (context) =>
+  layout(context, article(context, renderArticleContent()));
---
+++ source/partials/digest-widget/template.js
@@ -0,0 +1,40 @@
+module.exports = () => `
+<section class="digest-widget">
+  <h2 class="heading heading--3">Дайджест Форвеба</h2>
+
+  <p class="paragraph">
+    Во&nbsp;фронтенде много хайпа и&nbsp;информационного шума. Соцсети и&nbsp;медиа перегружают нас информацией и&nbsp;вызывают <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%B8%D0%BD%D0%B4%D1%80%D0%BE%D0%BC_%D1%83%D0%BF%D1%83%D1%89%D0%B5%D0%BD%D0%BD%D0%BE%D0%B9_%D0%B2%D1%8B%D0%B3%D0%BE%D0%B4%D1%8B">синдром упущенной выгоды</a>.
+  </p>
+
+  <p class="paragraph">
+    Дайджест Форвеба помогает бороться с&nbsp;этим: выходит раз в&nbsp;две недели и&nbsp;содержит только стоящие внимания события и&nbsp;материалы для фронтендеров.
+  </p>
+
+  <form
+    class="form"
+    action="https://forwebdev.us19.list-manage.com/subscribe/post?u=1e420147ab39ab295d24b7876&amp;id=3308e73dc7"
+    method="post"
+    target="_blank"
+  >
+    <div class="form__row form__row--inline">
+      <input class="input" type="email" name="EMAIL" id="email" placeholder="Эл. почта" required />
+
+      <div
+        style="position: absolute; left: -5000vw;"
+        aria-hidden="true"
+      >
+        <input
+          type="text"
+          name="b_1e420147ab39ab295d24b7876_3308e73dc7"
+          tabIndex="-1"
+          value=""
+        />
+      </div>
+
+      <button class="button" type="submit">
+        Подписаться
+      </button>
+    </div>
+  </form>
+</section>
+`;
