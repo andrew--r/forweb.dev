c0ed7c545764d57bf3a6b8e2bc106f0c020d64f1
Andrey Romanov <scorpion21.97@gmail.com>
2019-06-30 14:31:47 +0300

    Add new digest entry 2019-06-30

---
+++ source/pages/digest/2019-06-30/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicationDate: new Date('2019-06-30'),
+  title: 'Дайджест Форвеба №12',
+  description:
+    'Состояние CSS в 2019, дизайн-система ivi, замена Gulp от Microsoft, сервис-воркер в поиске Google, учебник по вёрстке, вопросы для собеседований, комбинаторы промисов, цена JavaScript в 2019',
+};
