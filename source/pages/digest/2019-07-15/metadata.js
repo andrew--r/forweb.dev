4152e12d4765a66dd1214afea528db0e4f5daa41
Andrey Romanov <scorpion21.97@gmail.com>
2019-07-14 13:11:42 +0300

    Add new digest entry 2019-07-15

---
+++ source/pages/digest/2019-07-15/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicationDate: new Date('2019-07-15'),
+  title: 'Дайджест Форвеба №13',
+  description:
+    'SOLID, история GraphQL, правила русской типографики, 9 лет в монолите, секрет хороших приложений на Elecrton, weak references, структурный поиск по коду, JS-движок для Android-приложений на React Native',
+};
