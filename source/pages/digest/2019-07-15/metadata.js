d5f96b67c5d0ca0f85a80335f65859e5ae262327
Andrey Romanov <scorpion21.97@gmail.com>
2019-07-15 08:42:25 +0300

    Fix typo

--- source/pages/digest/2019-07-15/metadata.js
+++ source/pages/digest/2019-07-15/metadata.js
@@ -2,5 +2,5 @@ module.exports = {
   publicationDate: new Date('2019-07-15'),
   title: 'Дайджест Форвеба №13',
   description:
-    'SOLID, история GraphQL, правила русской типографики, 9 лет в монолите, секрет хороших приложений на Elecrton, weak references, структурный поиск по коду, JS-движок для Android-приложений на React Native',
+    'SOLID, история GraphQL, правила русской типографики, 9 лет в монолите, секрет хороших приложений на Electron, weak references, структурный поиск по коду, JS-движок для Android-приложений на React Native',
 };
