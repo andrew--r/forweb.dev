078f9cfcfdf15e3aa6f0c0be3f4b13380b134749
Andrey Romanov <scorpion21.97@gmail.com>
2019-08-17 13:23:53 +0300

    Add new digest entry 2019-08-17

---
+++ source/pages/digest/2019-08-17/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicationDate: new Date('2019-08-17'),
+  title: 'Дайджест Форвеба №15',
+  description:
+    'Современный учебник JavaScript, Contact Picker API, Flow → TypeScript, ускорение веб-версии Instagram, спецификации, конкурс Telegram, двухфакторная аутентификация, Largest Contentful Paint, Network Information API',
+};
