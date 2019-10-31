275016fa2910e20e616a3c1e608e0e9456f18473
Andrey Romanov <scorpion21.97@gmail.com>
2019-10-31 17:52:18 +0300

    Add new digest entry 2019-10-31

---
+++ source/pages/digest/2019-10-31/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicationDate: new Date('2019-10-31'),
+  title: 'Дайджест Форвеба №20',
+  description:
+    'Убийство IE6, нужен ли GraphQL в эпоху HTTP/2, SMS Receiver API, современное руководство по Make, доступные палитры цветов, хитрости CSS-переменных, фреймворк инициализации Slack, revert и новый синтаксис display в CSS, ориентация изображений в вебе, PWA в Samsung Galaxy Store',
+};
