033aba48e49c472976370e7c29c26a7fe9d2afdc
Andrey Romanov <scorpion21.97@gmail.com>
2019-11-30 14:22:13 +0300

    Add new digest entry 2019-11-30

---
+++ source/pages/digest/2019-11-30/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicationDate: new Date('2019-11-30'),
+  title: 'Дайджест Форвеба №22',
+  description:
+    'Диаграммы состояний, фронтенд в GitLab, дизайн-система Airbnb, состояние спецификаций CSS, Deno, Redux Toolkit, ES-модули в Node.js',
+};
