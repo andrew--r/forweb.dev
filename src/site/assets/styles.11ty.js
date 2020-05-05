3614f5c76d71d5ff7d9248fd006760f864b033a1
Andrey Romanov <me@andreyromanov.com>
2020-05-05 10:31:04 +0300

    CSS → Sass, indicate visited links in articles
    closes #26

--- src/site/assets/styles.11ty.js
+++ src/site/assets/styles.11ty.js
@@ -1,28 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const sass = require('sass');
 const postcss = require('postcss');
 
 const fileName = 'styles.css';
-const rawFilepath = path.join(__dirname, `../_includes/styles/index.css`);
+const rawDir = path.resolve(__dirname, '../_includes/styles');
+const rawFilepath = path.resolve(rawDir, 'index.scss');
+const nodeModulesDir = path.resolve(__dirname, '../../../node_modules');
 
 module.exports = class {
   async data() {
     return {
       permalink: `assets/${fileName}`,
       rawFilepath,
-      rawCss: await fs.readFileSync(rawFilepath),
+      rawCss: (await fs.readFileSync(rawFilepath)).toString(),
     };
   }
 
   render({ rawCss, rawFilepath }) {
+    const compiledSass = sass.renderSync({
+      data: rawCss,
+      includePaths: [rawDir, nodeModulesDir],
+    });
+
     return postcss([
-      require('postcss-easy-import'),
-      require('postcss-nested'),
-      require('postcss-color-function'),
       require('postcss-selector-matches'),
       require('autoprefixer'),
     ])
-      .process(rawCss, { from: rawFilepath })
+      .process(compiledSass.css.toString(), {
+        from: rawFilepath.replace('scss', 'css'),
+      })
       .then((result) => result.css);
   }
 };
